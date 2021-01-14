665a3e004bd8533c9a2fa3e31d09a2465799080b
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-14 09:59:37 +0000

    http module implemented

--- express.js
+++ express.js
@@ -1,21 +1,11 @@
 const express = require('express');
-const { getLocalExternalIP } = require('./utility/network');
-
 const app = express();
 
-module.exports = (port) => {
-
-    if(!port){
-        throw new Error('[Enviroment Exception] Port Number is required!')
-    }
+module.exports = () => {
 
     app.get('/', (req, res) => {
         res.send('Maksitakip WEB API Services');
     });
 
-    app.listen(port, () => {
-        console.log(`${process.env.name} app listening at http://${getLocalExternalIP()}:${port}`)
-    });
-
-    
+    return app;
 }
--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-const express = require('./express');
+const app = require('./express');
+const http = require('http');
+const { getLocalExternalIP } = require('./utility/network');
+
+const runServer = async (port) => http.Server(app).listen(port, async error => {
+    if(error){
+        throw new Error('[Server] ' + error.message);
+    }
+
+    if(!port){
+        throw new Error('[Enviroment Exception] Port Number is required!')
+    }
+    
+    console.log(`${process.env.name} app listening at http://${getLocalExternalIP()}:${port}`)
+
+});
+
 
 (async function main(){
 
    
 
-    try {
-
-        
+    try {        
         const port =  process.env.PORT;
-        express(port);
-        
+        await runServer(port);
     } catch (error) {
-        console.log(error.message)
+        console.error(error.message)
     }
 
 })()
