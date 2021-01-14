4e769ef80ff52abfee01d019e06ae737f9d4e171
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-14 16:16:13 +0000

    middlewares added

--- express.js
+++ express.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const cors = require('cors');
+
 const app = express();
+
+app.use(morgan('common'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(compression());
+app.use(cors())
 
 
 app.get('/', (req, res) => {
