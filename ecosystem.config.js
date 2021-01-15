6442b30779f4f556e24f3cce006c28793881f277
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-15 17:08:39 +0000

    ds

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
       env: {
         "PORT": 3000,
         "NODE_ENV": "development",
-        "MONGODB_CONN_URI":"mongodb://localhost:27017/maksitakip",
+        "MONGODB_CONN_URI":"mongodb://localhost:27017,localhost:27018,localhost:27019/maksitakip?replicaSet=rs0",
         "MONGODB_DB_NAME": "maksitakip",
         "API_ROOT_KEY": "1234567890",
         "JWT_SECRET": "1234567890"
